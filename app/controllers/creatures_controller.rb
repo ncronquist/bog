ccfafc2cc9795bb987b4707d428d13011ea12b54
ncronquist <nick@ncronquist.com>
2015-04-29 16:58:01 -0700

    Update edit and delete routes and fix layout

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -6,10 +6,18 @@ class CreaturesController < ApplicationController
 
   def new
     @creature = Creature.new
+    @tags = Tag.all
   end
 
   def create
+
+    # render :json => params
+
     @creature = Creature.create(creature_params)
+
+    params['creature']['tag_ids'].each do |tag_id|
+      @creature.tags << Tag.find(tag_id) unless tag_id.blank?
+    end
     redirect_to @creature
   end
 
@@ -19,18 +27,23 @@ class CreaturesController < ApplicationController
 
   def edit
     @creature = Creature.find(params[:id])
+    @tags = Tag.all
   end
 
   def update
-    creature = Creature.find(params[:id])
-    creature.update!(creature_params)
-    redirect_to creature
+    @creature = Creature.find(params[:id])
+    @creature.update!(creature_params)
+
+    params['creature']['tag_ids'].each do |tag_id|
+      @creature.tags << Tag.find(tag_id) unless tag_id.blank?
+    end
+
+    redirect_to @creature
   end
 
   def destroy
-    creature = Creature.find(params[:id])
-    creature.delete!(creature_params)
-    redirect_to creatures
+    Creature.find(params[:id]).destroy
+    redirect_to creatures_path
   end
 
   private
