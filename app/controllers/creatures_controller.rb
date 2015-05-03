285515d78e6da8f8780169ebe60370ba4a6b85c0
ncronquist <nick@ncronquist.com>
2015-05-03 14:40:57 -0700

    Pull images from Flickr for each creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,3 +1,5 @@
+require 'flickraw'
+
 class CreaturesController < ApplicationController
 
   def index
@@ -22,7 +24,32 @@ class CreaturesController < ApplicationController
   end
 
   def show
+    # Set up the FlickRaw gem
+    FlickRaw.api_key=ENV["FLICKR_API_KEY"]
+    FlickRaw.shared_secret=ENV["FLICKR_API_SECRET"]
+
+    # Find the current creature
     @creature = Creature.find(params[:id])
+
+    # Retrieve a list of images that are relevant to the creature name
+    list = flickr.photos.search :text => @creature.name, :sort => "relevance"
+
+    # The list of images returns sorted by relevant
+    # Retrieve the 3 most relevant public photos
+    photolist = []
+    i = 0
+    until photolist.length == 3 do
+      photolist << list[i] if list[i].ispublic
+      i += 1
+    end
+
+    # For each photo retrieve the small version of the image
+    @photos = []
+    photolist.each do |photo|
+      sizes = flickr.photos.getSizes :photo_id => photo.id
+      # The fourth object is always the small picture
+      @photos << sizes[3]
+    end
   end
 
   def edit
